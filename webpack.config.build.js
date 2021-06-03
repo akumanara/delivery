2bf487e03cdcde0fad331588934f88f998de466a
thomas@antmoves.com <thomas@antmoves.comm>
2021-06-03 12:49:27 +0300

    disable monitor

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,6 +1,6 @@
 const path = require('path');
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const WebpackMonitor = require('webpack-monitor');
+// const WebpackMonitor = require('webpack-monitor');
 
 const { merge } = require('webpack-merge');
 
@@ -8,12 +8,12 @@ const config = require('./webpack.config');
 
 module.exports = merge(config, {
   mode: 'production',
-  plugins: [
-    new WebpackMonitor({
-      capture: true,
-      launch: true,
-    }),
-  ],
+  // plugins: [
+  //   new WebpackMonitor({
+  //     capture: true,
+  //     launch: true,
+  //   }),
+  // ],
   devtool: false,
   output: {
     path: path.join(__dirname, 'public'),
