d09b23144020b07c0dbcd5d6e87bb8da8bd94a0c
thomas@antmoves.com <thomas@antmoves.comm>
2021-06-04 15:25:45 +0300

    webpack production config fix

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,19 +1,33 @@
 const path = require('path');
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 // const WebpackMonitor = require('webpack-monitor');
+const webpack = require('webpack');
+const { GitRevisionPlugin } = require('git-revision-webpack-plugin');
+
+const gitRevisionPlugin = new GitRevisionPlugin();
 
 const { merge } = require('webpack-merge');
 
 const config = require('./webpack.config');
 
+const mode = 'production';
+
 module.exports = merge(config, {
-  mode: 'production',
+  mode,
   // plugins: [
   //   new WebpackMonitor({
   //     capture: true,
   //     launch: true,
   //   }),
   // ],
+  plugins: [
+    new webpack.DefinePlugin({
+      MODE: JSON.stringify(mode),
+      VERSION: JSON.stringify(gitRevisionPlugin.version()),
+      COMMITHASH: JSON.stringify(gitRevisionPlugin.commithash()),
+      BRANCH: JSON.stringify(gitRevisionPlugin.branch()),
+    }),
+  ],
   devtool: false,
   output: {
     path: path.join(__dirname, 'public'),
