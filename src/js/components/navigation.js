752429d6828bcbc06b46ac662e62c9634a83fe24
thomas@antmoves.com <thomas@antmoves.comm>
2021-07-02 11:43:34 +0300

    main nav

--- src/js/components/navigation.js
+++ src/js/components/navigation.js
@@ -8,28 +8,60 @@ export default class {
       toggleButton: document.querySelector('.header__burger'),
       mainNav: document.querySelector('.main-nav'),
       mainNavToggleButton: document.querySelector('.main-nav__top-burger-icon'),
+      userProfileModal: document.querySelector('.header__user-modal'),
+      userProfilePhoto: document.querySelector('.header__user-photo'),
+      userProfileBackground: document.querySelector('.header__user-modal-mask'),
     };
     this.isNavOpen = false;
     this.DOM.toggleButton.addEventListener('click', this.toggleMainNav);
     this.DOM.mainNavToggleButton.addEventListener('click', this.toggleMainNav);
+
+    this.isUserProfileOpen = false;
+    this.DOM.userProfilePhoto.addEventListener(
+      'click',
+      this.toggleProfileModal,
+    );
+    this.DOM.userProfileBackground.addEventListener(
+      'click',
+      this.toggleProfileModal,
+    );
   }
 
   toggleMainNav() {
     if (this.isNavOpen) {
-      this.closeNav();
+      this.closeMainNav();
     } else {
-      this.openNav();
+      this.openMainNav();
     }
     this.isNavOpen = !this.isNavOpen;
   }
 
-  closeNav() {
+  closeMainNav() {
     document.body.classList.remove('hide-overflow');
     this.DOM.mainNav.classList.remove('active');
   }
 
-  openNav() {
+  openMainNav() {
     document.body.classList.add('hide-overflow');
     this.DOM.mainNav.classList.add('active');
+  }
+
+  toggleProfileModal() {
+    if (this.isUserProfileOpen) {
+      this.closeProfileModal();
+    } else {
+      this.openProfileModal();
+    }
+    this.isUserProfileOpen = !this.isUserProfileOpen;
+  }
+
+  closeProfileModal() {
+    document.body.classList.remove('hide-overflow');
+    this.DOM.userProfileModal.classList.remove('active');
+  }
+
+  openProfileModal() {
+    document.body.classList.add('hide-overflow');
+    this.DOM.userProfileModal.classList.add('active');
   }
 }
